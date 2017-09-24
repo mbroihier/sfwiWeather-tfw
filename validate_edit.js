9f548e80f97d2829b5f35e5532ce98584c7eb08e
broihier <broihier@Zeta.local>
2017-09-24 13:20:58 -0500

    added validation logic to procedure step editing.

--- validate_edit.js
+++ validate_edit.js
@@ -1,5 +1,29 @@
-document.onmousedown = function(event) {
-  console.log("key is up: " + event);
-  console.log(event.explicitOriginalTarget.name);
-  console.log(event.explicitOriginalTarget.checked);
-}
+window.onload = function() {
+  let inputElements = document.getElementsByTagName("input");
+  let stepTextElements = document.getElementsByTagName("textarea");
+  let re = /step\d+/;
+  console.log("Number of input elements found: " + inputElements.length);
+  for (let element of inputElements) {
+    console.log("processing: " + element + " " + stepTextElements.length);
+    let textStepElement;
+    if (element.hasAttribute("name")) {
+      if (element.getAttribute("name").match(re)) {
+        textStepElement = stepTextElements[element.getAttribute("name").replace("step", "stepLine")];
+        element.onclick = function(event) { // note: use closure to preserve the state of textStepElement
+          console.log("got a click of a checkbox");
+          console.log("from: " + this.getAttribute("name"));
+          if (this.hasAttribute("checked")) {
+            this.removeAttribute("checked");
+            textStepElement.setAttribute("style", "text-decoration: line-through red");
+            console.log("turning off check");
+          } else {
+            this.setAttribute("checked", "");
+            textStepElement.removeAttribute("style");
+            console.log("turning on check");
+          }
+          console.log("checked: " + this.hasAttribute("checked"));
+        };
+      }
+    }
+  }
+};
